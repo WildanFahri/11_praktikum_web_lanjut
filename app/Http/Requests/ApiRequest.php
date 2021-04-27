ca6566cb0aad29111fe04e75530f6a32961a769d
WildanFahri <Wildan.fahri03@gmail.com>
2021-04-27 21:51:44 +0700

    update file ApiRequest.php

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -2,29 +2,31 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
 
-class ApiRequest extends FormRequest
+abstract class ApiRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    use ApiResponse;
+
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
     {
-        return false;
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    protected function failedAuthorization()
     {
-        return [
-            //
-        ];
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
     }
 }
